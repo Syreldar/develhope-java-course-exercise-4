97e56e7d124bae422bb4d46b026a047eae26143e
Syreldar <enrico.drago3@gmail.com>
2023-07-11 12:48:26 +0200

    Upload exercise

--- NumberOperations.java
+++ NumberOperations.java
@@ -2,5 +2,18 @@ public class NumberOperations
 {
     public static void main(String[] args)
     {
+        int a = 15;
+        a += 5;
+        a -= 4;
+
+        int b = a;
+        b++;
+
+        boolean firstCondition = b % 2 != 0;
+        boolean secondCondition = (b * (b + 1)) % 3 == 0;
+        if (firstCondition && secondCondition)
+            System.out.println("Both conditions are met.");
+        else
+            System.out.println("One or both conditions are not met.");
     }
 }
